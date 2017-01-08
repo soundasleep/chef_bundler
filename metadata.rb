386acc63dd4df9ae58bf361fa8192d8c4f2b3974
Jevon Wright <soundasleep@gmail.com>
2017-01-08 13:01:15 +0000

    Add name metadata to cookbook for Chef 12+
    Resolves error 'Chef::Exceptions::MetadataNotValid: Cookbook loaded at path(s) [...]
    has invalid metadata: The `name' attribute is required in cookbook metadata'.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name             'chef_bundler'
 maintainer       "Jevon Wright"
 maintainer_email "jevon@jevon.org"
 license          "MIT"
 description      "Provides lightweight resources and recipes for installing and working with Bundler."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 # Setup recipes.
